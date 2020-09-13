ec39c68eedf2137a8480d2ddb7010809a211fedc
Giovanni Calzolari <giovannicalzolari@outlook.com>
2020-09-13 13:25:09 +0200

    So basically I try to make the stones solid, which works a bit but mainly not, the thing is that when after 2 or three stones the program becomes super slow with a lot of mistakes and stuff whatsover, idk really the gio vector becomes very suddenly super long, so I should look into that, but it's a step forward

--- tetrisGio.py
+++ tetrisGio.py
@@ -1,7 +1,6 @@
 # Hello, this is my trial to code tetrisimport pygame
 import pygame
 import random
-import math
 #Initialize pygame
 pygame.init()
 
@@ -11,13 +10,13 @@ b = 600
 win = pygame.display.set_mode((a,b))
 pygame.display.set_caption('Tetris')
 
-
-def drawGrid():
-    bs = delta
-    for xx in range(a):
-        for yy in range(b):
-            rect = pygame.Rect(xx*bs,yy*bs,bs,bs)
-            pygame.draw.rect(win, (40,10,50), rect,2)
+# # this grid is too slow
+# def drawGrid():
+#     bs = delta
+#     for xx in range(a):
+#         for yy in range(b):
+#             rect = pygame.Rect(xx*bs,yy*bs,bs,bs)
+#             pygame.draw.rect(win, (40,10,50), rect,2)
 
 timer2 = 0
 clock = pygame.time.Clock()
@@ -29,13 +28,19 @@ delta = 20
 xf,yf,wf,hf = [[],[],[],[]]
 
 while run:
-    x = a/2
+    x = a//2
     y = 0
     w = bar[0]
     h = bar[1]
-#    pygame.draw.rect(win, (255,0,0), (x,y,w,h))
-    
-    terra = True
+    gio = {}
+    if len(xf) >= 1:
+        for z in range(b-delta,min(yf),-delta):
+            gio[z] = []
+            for i in range(len(xf)):
+                if yf[i]+hf[i] >= z:
+                    gio[z].append(range(xf[i],xf[i]+wf[i],delta))
+        
+    terra = True    
     while terra:
         
         win.fill((100,0,40))
@@ -54,7 +59,6 @@ while run:
                 if event.key == pygame.K_ESCAPE:
                     run = False
                     terra = False
-
 
                     
                 elif event.key == pygame.K_UP and y < b - delta - max(h,w):
@@ -85,20 +89,27 @@ while run:
         if keys[pygame.K_DOWN] and y < b - h - delta:
             y = y + delta
 
+        if len(xf) >= 1:
+            for z in range(b-delta,min(yf),-delta):
+                for i in range(len(gio[z])):
+                    for j in range(x,x+w,delta):
+                        if j in gio[z][i]:
+                            if y >= b - delta - h - hf[i]:
+                                terra = False
+                                xf.append(x)
+                                yf.append(y)
+                                wf.append(w)
+                                hf.append(h)
+
         if y >= b - delta - h:
             terra = False
             xf.append(x)
             yf.append(y)
             wf.append(w)
             hf.append(h)
-
-        
             
         pygame.draw.rect(win, (40,10,50), (0,0,a,b), 2*delta)
         pygame.display.update()
-
-    print(xf)
-
 
 
 pygame.quit()
